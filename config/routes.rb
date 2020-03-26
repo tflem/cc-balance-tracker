772e05c83347fb26f01774b1f9ab398d72fbc4de
Tim Fleming <tfleming26@hotmail.com>
2020-03-26 16:33:18 -0600

    Add nested routing helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "balances#index"
-
-
-  resources :balances
+  resources :balances do
+  	resources :issues
+  end
 end
